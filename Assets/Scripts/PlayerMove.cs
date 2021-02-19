b0ada9fb9d5e5903e2966cdd656a5d45664c2b93
RTXOFF <ljp6080@naver.com>
2021-02-19 23:05:31 +0900

    New TopDown Game Proj

---
+++ Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    Rigidbody2D rigid;
+
+    public float Speed;
+    float h;
+    float v;
+
+    private void Awake()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+    }
+    private void Update()
+    {
+        h = Input.GetAxisRaw("Horizontal");
+        v = Input.GetAxisRaw("Vertical");
+    }
+    private void FixedUpdate()
+    {
+        rigid.velocity = new Vector2(h, v) * Speed;
+    }
+}
