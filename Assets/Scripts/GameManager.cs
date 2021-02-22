6468a935344f619402424a18e7751966ccd70420
RTXOFF <ljp6080@naver.com>
2021-02-22 11:53:28 +0900

    Add Npc Dialogues & Portraits

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,22 +5,52 @@ using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
+    public TalkManager talkManager;
     public Text talkText;
     public GameObject talkPanel;
     public GameObject scanObject;
+    public Image portraitImg;
     public bool isAction;
+    public int talkIndex;
+
 
     public void Action(GameObject scanObj)
     {
-        if(isAction) //Exit Action
-            isAction = false;
-        else //Enter Action
-        {
-            isAction = true;
-            scanObject = scanObj;
-            talkText.text = scanObj.name + "이다.";
-        }
+
+        scanObject = scanObj;
+        ObjectData objData = scanObject.GetComponent<ObjectData>();
+        Talk(objData.id, objData.isNpc);
 
         talkPanel.SetActive(isAction);
+    }
+
+    void Talk(int id, bool isNpc)
+    {
+        string talkData = talkManager.GetTalk(id, talkIndex);
+
+        if (talkData == null)
+        {
+            isAction = false;
+            talkIndex = 0; //대화가 끝났으니 0으로 초기화
+            return; //void 함수에서 return은 강제 종료 역할
+        }
+
+        if(isNpc)
+        {
+            talkText.text = talkData.Split(':')[0];
+
+            portraitImg.sprite = talkManager.GetPortarit(id, int.Parse(talkData.Split(':')[1]));
+            //int.Parse() : 문자열을 해당 타입으로 변환해주는 함수.
+            portraitImg.color = new Color(1, 1, 1, 1);
+        }
+        else
+        {
+            talkText.text = talkData;
+
+            portraitImg.color = new Color(1, 1, 1, 0);
+        }
+
+        isAction = true;
+        talkIndex++;
     }
 }
---
+++ Assets/Scripts/TalkManager.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TalkManager : MonoBehaviour
+{
+    Dictionary<int, string[]> talkData; //Key - Value 한 쌍의 데이터 변수
+    Dictionary<int, Sprite> portraitData;
+
+    public Sprite[] portraitArr;
+
+    private void Awake()
+    {
+        talkData = new Dictionary<int, string[]>();
+        portraitData = new Dictionary<int, Sprite>();
+        GenerateData();
+    }
+
+    void GenerateData()
+    {
+        talkData.Add(1000, new string[] { "안녕?:0", "이 곳에 처음 왔구나?:2" }); //대화 하나에 여러 문장
+        talkData.Add(2000, new string[] { "난 이방인이 제일 싫어.:3", "넌 뭐야?:1" });
+        talkData.Add(100, new string[] { "평범한 나무상자다." });
+        talkData.Add(200, new string[] { "예쁜 꽃이다." });
+        
+        portraitData.Add(1000 + 0, portraitArr[0]); //0 Idle
+        portraitData.Add(1000 + 1, portraitArr[1]); //1 Talk
+        portraitData.Add(1000 + 2, portraitArr[2]); //2 Smile
+        portraitData.Add(1000 + 3, portraitArr[3]); //3 Angry
+
+        portraitData.Add(2000 + 0, portraitArr[4]);
+        portraitData.Add(2000 + 1, portraitArr[5]);
+        portraitData.Add(2000 + 2, portraitArr[6]);
+        portraitData.Add(2000 + 3, portraitArr[7]);
+
+    }
+
+    public Sprite GetPortarit(int id, int portraitIndex)
+    {
+        return portraitData[id + portraitIndex];
+    }
+
+    public string GetTalk(int id, int talkIndex)
+    {
+        if (talkIndex == talkData[id].Length)
+            return null;
+        else
+            return talkData[id][talkIndex];
+    }
+}
