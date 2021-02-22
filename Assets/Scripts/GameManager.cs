2c3bfab6c74204e51a2d42e558c2dab217ecc91b
RTXOFF <ljp6080@naver.com>
2021-02-22 22:44:11 +0900

    Add SubMenu & Save Function

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -11,15 +11,31 @@ public class GameManager : MonoBehaviour
     public Animator talkPanel;
     public Animator portraitAnim;
     public GameObject scanObject;
+    public GameObject menuSet;
+    public GameObject player;
     public Image portraitImg;
     public Sprite prevPortrait;
+    public Text questText;
 
     public bool isAction;
     public int talkIndex;
 
     private void Start()
     {
-        Debug.Log(questManager.CheckQuest());
+        GameLoad();
+        questText.text = questManager.CheckQuest();
+    }
+
+    private void Update()
+    {
+
+        if(Input.GetButtonDown("Cancel"))
+        {
+            if (menuSet.activeSelf)
+                menuSet.SetActive(false);
+            else
+                menuSet.SetActive(true);
+        }
     }
 
     public void Action(GameObject scanObj)
@@ -54,7 +70,7 @@ public class GameManager : MonoBehaviour
         {
             isAction = false;
             talkIndex = 0; //대화가 끝났으니 0으로 초기화
-            Debug.Log(questManager.CheckQuest(id));
+            questText.text = questManager.CheckQuest(id);
             return; //void 함수에서 return은 강제 종료 역할
         }
         //Continue Talk
@@ -83,5 +99,46 @@ public class GameManager : MonoBehaviour
 
         isAction = true;
         talkIndex++;
+    }
+
+    public void GameSave()
+    {
+        //player X
+        PlayerPrefs.SetFloat("PlayerX", player.transform.position.x);
+
+        //player Y
+        PlayerPrefs.SetFloat("PlayerY", player.transform.position.y);
+
+        //Quest Id
+        PlayerPrefs.SetInt("QuestId", questManager.questId);
+
+        //Quest Action Index
+        PlayerPrefs.SetInt("QuestActionIndex", questManager.questActionIndex);
+
+        //PlayerPrefs 기능 이용, Company, Product Name 으로 컴퓨터 레지스트리에 저장
+        PlayerPrefs.Save();
+        
+        menuSet.SetActive(false);
+    }
+
+    public void GameLoad()
+    {
+        if (!PlayerPrefs.HasKey("PlayerX"))
+            return;
+
+        float x = PlayerPrefs.GetFloat("PlayerX");
+        float y = PlayerPrefs.GetFloat("PlayerY");
+        int questId = PlayerPrefs.GetInt("QuestId");
+        int questActionIndex = PlayerPrefs.GetInt("QuestActionIndex");
+
+        player.transform.position = new Vector2(x, y);
+        questManager.questId = questId;
+        questManager.questActionIndex = questActionIndex;
+        questManager.ControlObject();
+    }
+
+    public void GameExit()
+    {
+        Application.Quit(); //게임 종료 함수, 에디터에서는 실행되지 않음
     }
 }
